e64ba3c0e833b35f4bf52c99314814fb9253af19
2505621313@qq.com <2505621313@qq.com>
2022-01-16 15:20:20 +0800

    基本完成

--- main.js
+++ main.js
@@ -2,21 +2,26 @@ let currentTime = new Date() //new Date()
 render(currentTime)
 
 g('#prevMonth').onclick = () => {
-    render(new Date(currentTime - 86400 * 1000 * 30))
+    const 月初 = new Date(currentTime.getFullYear(), currentTime.getMonth(), 1)
+    render(new Date(月初 - 86400 * 1000))
+
 }
 g('#nextMonth').onclick = () => {
-    render(new Date(currentTime - 0 + 86400 * 1000 * 30))
+    const 下月初 = new Date(currentTime.getFullYear(), currentTime.getMonth() + 1, 1)
+    render(下月初)
 }
 g('#today').onclick = () => {
-    render(new Date())
-}
-
+        render(new Date())
+    }
+    //帮助函数
 function g(selector) {
     return document.querySelector(selector)
 }
+
 function gs(selector) {
     return document.querySelectorAll(selector)
 }
+
 function render(time) {
     const year = time.getFullYear()
     const month = time.getMonth() + 1
@@ -24,10 +29,12 @@ function render(time) {
     initTime()
     generateDays()
     currentTime = time
+
     function initTime() {
         const time = g('#time')
         time.textContent = `${year}年${month}月`
     }
+
     function generateDays() {
         const 月初 = new Date(year, month - 1, 1)
         const 月初星期几 = 月初.getDay()
@@ -37,25 +44,44 @@ function render(time) {
 
         const days = g('#days')
         days.innerHTML = ""
+        let n = 0
         for (let i = 1; i < 月初星期几; i++) {
             const li = document.createElement('li')
             const d = new Date(月初 - 86400 * 1000 * i)
             li.textContent = d.getDate()
+            li.classList.add('calendar-days-disabled')
             days.prepend(li)
+            n += 1
         }
 
         const 这个月多少天 = 月末几号
+        const now = new Date()
+        let selectedLi
         for (let i = 1; i <= 这个月多少天; i++) {
             const li = document.createElement('li')
             li.textContent = i
+            if (i === now.getDate() && month === now.getMonth() + 1 && year === now.getUTCFullYear()) {
+                li.classList.add("calendar-days-today")
+            }
+            li.onclick = () => {
+                if (selectedLi) { selectedLi.classList.remove('calendar-days-selected') }
+                li.classList.add("calendar-days-selected")
+                selectedLi = li
+            }
             days.append(li)
+            n += 1
+
         }
-        for (let i = 月末星期几 + 1; i <= 7; i++) {
+        let i = 月末星期几 + 1
+        for (let j = 0; j < 42 - n; j++) {
             const delta = i - 月末星期几
             const li = document.createElement('li')
             const d = new Date(月末 - 0 + 86400 * 1000 * delta)
             li.textContent = d.getDate()
             days.append(li)
+            li.classList.add('calendar-days-disabled')
+
+            i++
         }
 
     }
